89755320dd1fdecb3bf0692793c4d400ddff4001
sryang <sarang628@gmail.com>
2026-03-06 15:22:49 +0900

    update RestaurantDetailColumn

--- providePullToRefresh.kt
+++ providePullToRefresh.kt
@@ -9,7 +9,7 @@ import com.sryang.library.pullrefresh.RefreshIndicatorState
 
 data class PullToRefreshData(
     val modifier : Modifier = Modifier,
-    val state : RefreshIndicatorState = RefreshIndicatorState.Default,
+    val state : com.sarang.torang.compose.feed.state.RefreshIndicatorState = com.sarang.torang.compose.feed.state.RefreshIndicatorState.Default,
     val onRefresh: (() -> Unit) = {},
     val contents: @Composable () -> Unit = {}
 )
@@ -18,10 +18,10 @@ fun providePullToRefresh(state: PullToRefreshLayoutState): @Composable (PullToRe
     { data ->
         state.updateState(
             when(data.state){
-                RefreshIndicatorState.Default -> RefreshIndicatorState.Default
-                RefreshIndicatorState.PullingDown -> RefreshIndicatorState.PullingDown
-                RefreshIndicatorState.ReachedThreshold -> RefreshIndicatorState.ReachedThreshold
-                RefreshIndicatorState.Refreshing -> RefreshIndicatorState.Refreshing
+                com.sarang.torang.compose.feed.state.RefreshIndicatorState.Default -> RefreshIndicatorState.Default
+                com.sarang.torang.compose.feed.state.RefreshIndicatorState.PullingDown -> RefreshIndicatorState.PullingDown
+                com.sarang.torang.compose.feed.state.RefreshIndicatorState.ReachedThreshold -> RefreshIndicatorState.ReachedThreshold
+                com.sarang.torang.compose.feed.state.RefreshIndicatorState.Refreshing -> RefreshIndicatorState.Refreshing
             })
 
         PullToRefreshLayout(modifier                = data.modifier,
